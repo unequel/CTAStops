8e57a2f82e57f1ef22e1bc60598717a5dfec00a9
infiniti.spaw@gmail.com <>
2014-08-31 02:59:58 +0000

    init

---
+++ CTAStops-GitHub-master/js/markers.js
@@ -0,0 +1,33 @@
+$( document ).on( "pageinit", "#map-page", function() {
+    var defaultLatLng = new google.maps.LatLng( 41.87,-87.62 );
+    if ( navigator.geolocation ) {
+        function success(pos) {
+            
+            drawMap(new google.maps.LatLng(pos.coords.latitude, pos.coords.longitude));
+        }
+        function fail(error) {
+            drawMap(defaultLatLng);  
+        }
+        
+        navigator.geolocation.getCurrentPosition(success, fail, {maximumAge: 500000, enableHighAccuracy:true, timeout: 6000});
+    } else {
+        drawMap(defaultLatLng);
+    }
+    function drawMap(latlng) {
+        var myOptions = {
+            zoom: 10,
+            center: latlng,
+            mapTypeId: google.maps.MapTypeId.ROADMAP
+        };
+        var map = new google.maps.Map(document.getElementById("map-canvas"), myOptions);
+        
+        var marker = new google.maps.Marker({
+            position: latlng,
+            map: map  
+        });
+    }
+});
+
+  $(document).ready(function(){
+    console.log("mainmap");
+  });
---
+++ CTAStops-GitHub-master/js/pull2.js
@@ -0,0 +1,34 @@
+$.getJSON('js/busstops.json', function(busstops){
+  $('#busstoplist').empty();
+  
+  $.each(busstops, function(i, busstop){
+    $('#busstoplist').append(generateBusStopLink(busstop));
+  });
+  $('#busstoplist').listview('refresh');
+  });
+function generateBusStopLink(busstop){
+  return'<li><a href="javascript:void(0)'
+    + '"onclick="goToStopDetailPage(\''
+    + busstop.stop_name
+    + '\',\''
+    + busstop.station_descriptive_name +'\')">'
+    + busstop.stop_name
+    + '</a></li>';
+}
+
+  function goToStopDetailPage(stopName){
+
+  //create the page html template
+  var stopPage = $("<div data-role='page' data-url=station><div data-role='header'><h1>"
+                  + stopName + "</h1></div><div data-role='content'></div><div data-role='footer'><h4>"
+                  + stopName + "</h4></div></div>");
+  //append the new page to the page container
+  stopPage.appendTo( $.mobile.pageContainer);
+  
+  //go to the newly created page
+  $.mobile.changePage( stopPage);
+  }
+  
+  $(document).ready(function(){
+    console.log("main2");
+  });
